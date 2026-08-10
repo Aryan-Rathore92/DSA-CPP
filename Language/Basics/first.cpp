38b02d246485d372c04bd8425c332b77df87a0b4
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-08-11 00:05:37 +0530

    Basics of language

---
+++ Language/Basics/first.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   /* int a = 123;
+    char b = 'b';
+    bool c = true;
+    float d = 1.2;
+    double e = 1.23;
+
+    int sizeA = sizeof(a);
+    int sizeB = sizeof(b);
+    int sizeC = sizeof(c);
+    int sizeD = sizeof(d);
+    int sizeE = sizeof(e);
+
+    cout << sizeA << endl;
+    cout << sizeB << endl;
+    cout << sizeC << endl;
+    cout << sizeD << endl;
+    cout << sizeE << endl; */
+    
+    // Type Casting
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+}
