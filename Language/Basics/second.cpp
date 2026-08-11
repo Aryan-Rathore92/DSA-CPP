e5a600fdffc2445f147dbc32a48d6a9f4441ab83
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-08-11 23:41:07 +0530

    learn if else

---
+++ Language/Basics/second.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a;
+    // a = cin.get();// Get Character and all keys
+    //  cout << "Value of a is: " << a;
+
+    int b;
+    cout << "Enter the value of b: ";
+    cin>>b;
+
+    if( b > 0){
+        cout << "Number is Positive";
+    }
+    
+
+
+}
