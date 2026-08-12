18283c56dcf3ee68760b2d27b846f4fdd41eab1f
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-08-12 23:34:36 +0530

    while loop end

--- Language/Basics/three.cpp
+++ Language/Basics/three.cpp
@@ -3,36 +3,36 @@ using namespace std;
 
 int main(){
     // Question-01
-    int n, sum=0, i=1;
-    cout << "Enter the value of n: ";
-    cin>> n;
+    // int n, sum=0, i=1;
+    // cout << "Enter the value of n: ";
+    // cin>> n;
 
-    while(n>=i){
-        sum += i;
-        i++;
-    }
-    cout << sum;
+    // while(n>=i){
+    //     sum += i;
+    //     i++;
+    // }
+    // cout << sum;
     
     // Question-02
-    int n, sum=0, i=2;
-    cout << "Enter the value of n : ";
-    cin>>n;
+    // int n, sum=0, i=2;
+    // cout << "Enter the value of n : ";
+    // cin>>n;
 
-    while (n>=i)
-    {
-        if(i%2==0){
-            sum += i;
-        }
-        i = i+2;
-    }
-    cout << sum <<endl;
+    // while (n>=i)
+    // {
+    //     if(i%2==0){
+    //         sum += i;
+    //     }
+    //     i = i+2;
+    // }
+    // cout << sum <<endl;
 
     // Question-03
-    float f, c;
-    cout << "Enter the value of temprature in fahrenheit : ";
-    cin>>f;
-    c = (f - 32)*5/9;
-    cout << "The value of tempreatute in celsius is : " << c <<endl;
+    // float f, c;
+    // cout << "Enter the value of temprature in fahrenheit : ";
+    // cin>>f;
+    // c = (f - 32)*5/9;
+    // cout << "The value of tempreatute in celsius is : " << c <<endl;
 
     
     // Question-04
@@ -48,6 +48,9 @@ int main(){
             cout << "Number is prime : "<< i << endl;
         }
         i++;
+    }
+    if(n==i){
+        cout << "Prime is : "<< i << endl;
     }
     
 }
