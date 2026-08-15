7f26429d13ea035ff9b19ed2ddd0313e0a911bf8
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-08-15 23:33:13 +0530

    27 pattern complete

---
+++ Language/Patterns/six.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Pattern --- 21
+    // int n, row=1;
+    // cin>>n;
+    // while(n>=row){
+    //     int col = 1;
+    //     int space = n-row;
+    //     while(space){ // Print spaces
+    //         cout<< " "; 
+    //         space -= 1;
+    //     }
+    //     while(row>=col){ // Print stars
+    //         cout << "*";
+    //         col++;
+    //     }
+    //     cout << endl;
+    //     row++;
+    // }
+
+    /*   // Pattern --- 22
+       int n,i=1;
+       cin>>n;
+       while(n>=i){
+        int j=1;
+        while((n-i+1)>=j){
+            cout << "*";  // {Method---01}
+            j++;
+        }
+        cout << endl;
+        i++;
+       }
+       int n,i=1;
+       cin>>n;
+       while(n>=i){
+        int j=i;
+        while(n>=j){
+            cout << "*"; // {Method---02}
+            j++;
+        }
+        cout << endl;
+        i++;
+       }
+    */
+
+    // Pattern --- 23
+    // int n,i=1;
+    // cin>>n;
+    // while(n>=i){
+    //     int space = i-1;
+    //     int j=1;
+    //     while(space){
+    //         cout << " ";
+    //         space -= 1;
+    //     }
+    //     while((n-i+1)>=j){
+    //        cout << "*";
+    //        j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // Pattern --- 24
+    //    int n,i=1;
+    //    cin>>n;
+    //    while(n>=i){
+    //     int j=1;
+    //     int space = i-1;
+    //     while(space){
+    //        cout << " ";
+    //        space -= 1;
+    //     }
+    //     while((n-i+1)>=j){
+    //         cout << i;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    //    }
+
+    // Pattern --- 25
+    //    int n, i=1;
+    //    cin>>n;
+    //    while(n>=i){
+    //     int j=1;
+    //     int space = n-i;
+    //     while(space){
+    //         cout<< " ";
+    //         space -= 1;
+    //     }
+    //     while(i>=j){
+    //         cout << i;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    //    }
+
+    // Pattern --- 26
+    //    int n,i=1;
+    //    cin>>n;
+    //    while(n>=i){
+    //     int j=i;
+    //     int space = i-1;
+    //     while(space){
+    //         cout << " ";
+    //         space -= 1;
+    //     }
+    //     while(n>=j){
+    //         cout << j;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    //    }
+
+    // Pattern --- 27
+    //    int n,i=1,value=1;
+    //    cin>>n;
+    //    while(n>=i){
+    //     int j=1;
+    //     int space = n-i;
+    //     while(space){
+    //         cout << " ";
+    //         space -= 1;
+    //     }
+    //     while(i>=j){
+    //         cout << value;
+    //         value += 1;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    //  }
+       
+}
