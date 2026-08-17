37fb9ba5f64ef4de6a8ddfc83b800dd1c48677c4
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-08-17 12:07:08 +0530

    fibbonaci series and for loop questions

---
+++ Language/Operators/second.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // int n;
+    // cin>>n;
+    // for(int i=1; n>=i; i++){
+    //     cout << i << endl;
+    // }
+
+    // Question---01
+    //    int n,sum=0;
+    //    cin>>n;
+    //    for(int i=1; n>=i; i++){
+    //     sum += i;
+    //    }
+    //    cout << "Sum of "<< n << " numbers is: " << sum << endl;
+
+    // Question--02 (Fibbonaci series)
+    //   int n;
+    //   int a = 0;
+    //   int b = 1;
+    //   cout << "Enter the limit of fibbonaci series: ";
+    //   cin>>n;
+
+    //   for(int i=1; n>=i; i++){
+    //     int nextTerm = a + b;
+    //     cout << nextTerm << " ";
+    //     a=b;
+    //     b= nextTerm;
+    //   }
+
+    // Question---03 (Check number is prime or not)
+    //   int n;
+    //   bool isPrime = 1;
+    //   cout << "Enter the value of n: ";
+    //   cin>>n; 
+    //   for(int i=2; i<n; i++){
+    //     if(n%i==0){
+    //       isPrime = 0;
+    //       break;
+    //     }
+    //   }
+
+    //   if(isPrime == 0){
+    //     cout <<n<< " Not a prime number" << endl;
+    //   }else{
+    //     cout <<n<< " is a prime number" << endl;
+    //   }
+
+    // Question---04
+    //    for(int i=1; i<=5; i++){
+    //     cout << i << " "; // 1 3 5 
+    //     i++;
+    //    }
+
+    // Question---05
+      
+      
+
+
+}
