d95f60ecf408c8cd88ba143afd463bd76df55801
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-08-23 11:53:52 +0530

    first learning of array complete

--- Array/first.cpp
+++ Array/first.cpp
@@ -1,15 +1,34 @@
 #include<iostream>
 using namespace std;
 
-int nums[10];
+int arr[5] = {1,2,3,4,5};
+void printArray(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+
 int main(){
     
     // std::fill(name, name+1000000, 5); // This will fill entire array with a number is 5
     // cout << name[5]<< endl;
-    // cout << name[1]<< endl;
-    // cout << name[10]<< endl;
-    std::fill(nums, nums+10, 5);
-    for(int i=0; i<10; i++){
-        cout << nums[i] << " ";
-    }
+
+    // fill complete array with same element
+    // std::fill(nums, nums+10, 5);
+    // for(int i=0; i<10; i++){
+    //     cout << nums[i] << " ";
+    // }
+
+    // Calculate size of array
+    // int size = sizeof(nums)/sizeof(int);
+    // cout << size << endl;
+
+    int size = sizeof(arr)/sizeof(int);
+    printArray(arr,size); // 1 2 3 4 5 
+
+    double firstdouble[5];
+    float firstFloat[5];
+    bool firstBool[5];
+
 }
